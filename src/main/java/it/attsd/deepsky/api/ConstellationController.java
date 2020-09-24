febdbff088438c5f1336e5e0348cf58937e591bf
morganteg <gabriele.morgante@resiltech.com>
2020-09-24 16:57:48 +0200

    Travis config

--- src/main/java/it/attsd/deepsky/api/ConstellationController.java
+++ src/main/java/it/attsd/deepsky/api/ConstellationController.java
@@ -4,8 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import it.attsd.deepsky.entity.Constellation;
-import it.attsd.deepsky.exception.RepositoryException;
 import it.attsd.deepsky.model.ConstellationRepository;
 
 @RestController
