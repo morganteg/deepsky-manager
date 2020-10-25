f8f53652806377adec553fad72fc5de1115febe5
Gabriele Morgante <gabriele.morgante83@gmail.com>
2020-10-25 09:00:15 +0100

    e2e test added

--- src/test/java/it/attsd/deepsky/e2e/HomeE2ETest.java
+++ src/test/java/it/attsd/deepsky/e2e/HomeE2ETest.java
@@ -18,27 +18,27 @@ public class HomeE2ETest {
 	private static String baseUrl = "http://localhost:" + port;
 	private WebDriver driver;
 
-//	@BeforeClass
-//	public static void setupClass() {
-//		WebDriverManager.chromedriver().setup();
-//	}
-//
-//	@Before
-//	public void setup() {
-//		baseUrl = "http://localhost:" + port;
-//		driver = new ChromeDriver();
-//	}
-//
-//	@After
-//	public void teardown() {
-//		driver.quit();
-//	}
-//
-//	@Test
-//	public void testHomePage() {
-//		driver.get(baseUrl);
-//		WebElement element = driver.findElement(By.cssSelector("h1[id*='title"));
-//		
-//		assertThat(element.getText().equalsIgnoreCase("home"));
-//	}
+	@BeforeClass
+	public static void setupClass() {
+		WebDriverManager.chromedriver().setup();
+	}
+
+	@Before
+	public void setup() {
+		baseUrl = "http://localhost:" + port;
+		driver = new ChromeDriver();
+	}
+
+	@After
+	public void teardown() {
+		driver.quit();
+	}
+
+	@Test
+	public void testHomePage() {
+		driver.get(baseUrl);
+		WebElement element = driver.findElement(By.cssSelector("h1[id*='title"));
+		
+		assertThat(element.getText().equalsIgnoreCase("home"));
+	}
 }
